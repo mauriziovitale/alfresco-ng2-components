74104f6efe966667ad435f65893c4ac1be98d415
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-11-19 11:11:26 +0000

    test job

--- scripts/travis-job.sh
+++ scripts/travis-job.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-GITHUB_TOKEN="57a47ddab5708846b7e43ceefb75aaa3376c3e45"
+GITHUB_TOKEN=""
 
 #TRAVIS_POST_TOKEN="$( curl -X POST \
 #  https://api.travis-ci.org/auth/github \
 #  -H 'Accept: application/json' \
 #  -H 'Content-Type: application/json' \
-#  -d '{"github_token":"57a47ddab5708846b7e43ceefb75aaa3376c3e45"}' 2> /dev/null )"
+#  -d '{"github_token":""}' 2> /dev/null )"
 #
 #TRAVIS_TOKEN=(`echo $TRAVIS_POST_TOKEN | sed -e 's/[{}]/''/g' | sed -e 's/[{"access_token":}]/''/g' `)
 
